417cbce0ec8697aafcb29c81b2b5d37a2293ead6
Robert Young, Jr <jyoung96@fingerlakes.edu>
2026-02-28 19:23:03 +0000

    C_to_F Again

--- convert_C_to_F_02.py
+++ convert_C_to_F_02.py
@@ -1,8 +1,8 @@
 # FILE NAME - convert_C_to_F_02.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Robert Young
+# DATE: 02/28/2026
+# BRIEF DESCRIPTION: Create module that asks a user to convert temp from C to F or F to C. Then asks for the Temp.
 
 
 
@@ -14,21 +14,27 @@
 
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
-
-
-
-
-
-
-
-
-
+def main():
+    def convert_temp():
+        # c to f = temperature * 9/5 + 32
+        # f to c = (temperature - 32 ) * 5/9
+      print("===== Temperature Converter =====") 
+      print()
+      print(" 1. Convert from Celsius to Fahrenheit")
+      print(" 2. Convert from Fahrenheit to Celsius")
+      print()
+      choice = int(input("Please choose from the above menu: "))
+      temperature = int(input("Enter a temperature to convert: "))
+      if choice == 1:
+         convert_temp = temperature * 9/5 + 32
+         print(f"{temperature} degrees Celsius is {convert_temp} degrees Fahrenheit.")
+      elif choice == 2:
+        convert_temp = (temperature - 32) * 5/9
+        print(f"{temperature} degrees Fahrenheit is {convert_temp} degrees Celsius.")
+    convert_temp()
+main()
 
 ########### END YER CODE ABOVE THIS LINE ###########
-
-    
-
-
 
 ########################################
 #          SAMPLE OUTPUT
@@ -93,10 +99,7 @@ Enter a temperature to convert: -40
 
 1. What is one lesson you learned in this lab?
 
-
-
-
-
+I didn't really learn anything new, just getting into the flow of writing code and inmporting previous modules. 
 
 
 '''
