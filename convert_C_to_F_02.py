634c69453da339f93e8376edde20412abbc985df
Robert Young, Jr <jyoung96@fingerlakes.edu>
2026-02-28 19:46:46 +0000

    C_2_F_2

--- convert_C_to_F_02.py
+++ convert_C_to_F_02.py
@@ -24,11 +24,11 @@ def main():
       print(" 2. Convert from Fahrenheit to Celsius")
       print()
       choice = input("Please choose from the above menu: ")
-      temperature = input("Enter a temperature to convert: ")
-      if choice == "1":
+      temperature = float(input("Enter a temperature to convert: "))
+      if choice == 1:
          convert_temp = temperature * 9/5 + 32
          print(f"{temperature:.1f} degrees Celsius is {convert_temp:.1f} degrees Fahrenheit.")
-      elif choice == "2":
+      elif choice == 2:
         convert_temp = (temperature - 32) * 5/9
         print(f"{temperature:.f} degrees Fahrenheit is {convert_temp:.1f} degrees Celsius.")
     convert_temp()
